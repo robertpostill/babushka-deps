decb18b2d0efda7d1d262dfb4a1187bd003fcb67
Robert Postill <robert@grinning-cat.com>
2012-01-25 22:21:40 +1100

    Extended to un and install.

--- ohai.rb
+++ ohai.rb
@@ -1,4 +1,11 @@
-dep 'ohai' do
+dep 'ohai installed' do
   met? { `which ohai` =~ /ohai/ }
   meet { `gem install ohai` }
 end
+
+dep 'ohai' do
+  requires 'ohai installed'
+  met? { `which ohai` =~ /ohai/ }
+  meet { `ohai` }
+end
+
